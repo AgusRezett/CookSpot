7495bfc5fbfb8e9167f2c89422ad686e1327eaac
Agustin Rezett <agustin.rezett@gmail.com>
2023-04-25 17:18:20 -0300

    change: cambio de proyecto a react

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import LandingPage from './pages/LandingPage';
+import AddRecipe from './components/AddRecipe';
+import RecipeDetails from './components/RecipeDetails';
+
+function App() {
+	return (
+		<BrowserRouter>
+			<Switch>
+				<Route exact path="/" component={LandingPage} />
+				<Route path="/home" exact component={HomePage} />
+				<Route path="/recipe" exact component={AddRecipe} />
+				<Route path="/home/:id" component={RecipeDetails} />
+			</Switch>
+		</BrowserRouter>
+	);
+}
+
+export default App;
