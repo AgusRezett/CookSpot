311e02dee2e75757b3c4057299809e5206f3a6e5
Agustin Rezett <agustin.rezett@gmail.com>
2023-05-03 19:54:21 -0300

    release: react project base reinitiaded

--- client/src/App.js
+++ client/src/App.js
@@ -1,44 +1,25 @@
-import { useEffect, useState } from 'react';
-import { BrowserRouter, Route, Switch, useLocation } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Landing from './pages/Landing';
-import Home from './pages/Home';
-import Recipes from './pages/Recipes';
-import AddRecipe from './components/AddRecipe';
-import RecipeDetails from './pages/RecipeDetails';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-	const location = new useLocation();
-	const [mainContainerPaddingTop, setMainContainerPaddingTop] = useState('');
-	const [showNavbar, setShowNavbar] = useState(true);
-
-	const paddingMainContainerStyles = {
-		padding: 20,
-		paddingTop: showNavbar
-			? mainContainerPaddingTop === 40
-				? mainContainerPaddingTop + 50
-				: mainContainerPaddingTop + 25
-			: 20,
-	};
-
-	useEffect(() => {
-		setShowNavbar(location.pathname !== '/');
-	}, [location.pathname]);
-
-	return (
-		<BrowserRouter>
-			{showNavbar && <Navbar applyHeight={setMainContainerPaddingTop} />}
-			<div className="main-container" style={paddingMainContainerStyles}>
-				<Switch>
-					<Route exact path="/" component={Landing} />
-					<Route path="/home" exact component={Home} />
-					<Route path="/recipe" exact component={AddRecipe} />
-					<Route path="/recipes" exact component={Recipes} />
-					<Route path="/home/:id" component={RecipeDetails} />
-				</Switch>
-			</div>
-		</BrowserRouter>
-	);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
-import store from './store/index';
-import { BrowserRouter } from 'react-router-dom';
-import './styles/globals.css';
-import { AnimatePresence } from 'framer-motion';
 
-ReactDOM.render(
-	<React.StrictMode>
-		<Provider store={store}>
-			<BrowserRouter>
-				<AnimatePresence>
-					<App />
-				</AnimatePresence>
-			</BrowserRouter>
-		</Provider>
-	</React.StrictMode>,
-	document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
