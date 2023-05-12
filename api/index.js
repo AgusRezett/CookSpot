bf5c9f41e979eb89865b3a5b34cab038abdab61b
Agustin Rezett <agustin.rezett@gmail.com>
2023-05-11 23:30:56 -0300

    test: credentials log

--- api/index.js
+++ api/index.js
@@ -1,9 +1,15 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
+const { PGUSER, PGPASSWORD, PGHOST, PGDATABASE, API_KEY } = process.env;
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
 	server.listen(3001, () => {
+		console.log(`%s ${PGUSER}`);
+		console.log(`%s ${PGPASSWORD}`);
+		console.log(`%s ${PGHOST}`);
+		console.log(`%s ${PGDATABASE}`);
+		console.log(`%s ${API_KEY}`);
 		console.log('%s listening at 3001');
 	});
 });
